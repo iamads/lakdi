5a3ed217311fa0befa9892deccc88b2a8ff31883
ads <abhijeet3192@gmail.com>
2016-04-02 18:19:46 +0530

    Added gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp'),
+    nodemon = require('gulp-nodemon');
+
+gulp.task('default', function(){
+    nodemon({
+        script: 'index.js',
+        exr: 'js',
+        env: {
+            PORT: 3000
+        },
+        ignore: ['./node_modules/**']
+    })
+    .on('restart', function(){
+        console.log('Restarting');
+    });
+})
